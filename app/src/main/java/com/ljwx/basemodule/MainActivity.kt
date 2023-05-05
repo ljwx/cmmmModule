e894ca3fa3ecd2e2a6cb9e7ce8ae60c67122df43
ljwx <1146699396@qq.com>
2023-05-05 12:19:45 +0000

    update app/src/main/java/com/ljwx/basemodule/MainActivity.kt.
    Signed-off-by: ljwx <1146699396@qq.com>

--- app/src/main/java/com/ljwx/basemodule/MainActivity.kt
+++ app/src/main/java/com/ljwx/basemodule/MainActivity.kt
@@ -6,13 +6,16 @@ import com.ljwx.baseapp.BaseViewModel
 import com.ljwx.basemodule.databinding.ActivityMainBinding
 
 class MainActivity : FastMainActivity<ActivityMainBinding, BaseViewModel>(R.layout.activity_main) {
+
+    override fun getTabLayout() = mBinding.tabLayout
+
+    override fun getViewPager2() = mBinding.viewPager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        mTabLayout = mBinding.tabLayout
-        mViewPager = mBinding.viewPager
         addTabFragment("loadmore", RecyclerViewFragment())
-        attachTabLayoutViewPager()
+        addTabFragment("javaTest", TestJavaFragment(0))
 
     }
 }
