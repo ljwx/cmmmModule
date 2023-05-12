11d2c53ac1fdf3906dfec218a5eac6f3c1e35cab
ljwx <1146699396@qq.com>
2023-05-12 10:17:21 +0000

    app

--- app/src/main/java/com/ljwx/basemodule/MainActivity.kt
+++ app/src/main/java/com/ljwx/basemodule/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : FastMainActivity<ActivityMainBinding, BaseViewModel>(R.layo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        addTabFragment("basefragment", BaseFragmentTest())
         addTabFragment("loadmore", RecyclerViewFragment())
         addTabFragment("javaTest", TestJavaFragment(0))
 
