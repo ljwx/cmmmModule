0ebe16f21c14697fab722aebf02dc3dc384d9858
ljwx <1146699396@qq.com>
2023-05-15 20:24:04 +0800

    Create LogLocationUtils.java
    添加日志工具

---
+++ baseapp/src/main/java/com/ljwx/baseapp/LogLocationUtils.java
@@ -0,0 +1,17 @@
+package com.ljwx.baseapp;
+
+import android.util.Log;
+
+public class LogLocationUtils {
+
+    public static void d(String tag, String info) {
+        Throwable throwable = new Throwable();
+        String className = throwable.getStackTrace()[1].getClassName();
+        className = className.substring(className.lastIndexOf(".")+1);
+        StackTraceElement[] elements = throwable.getStackTrace();
+        String methodName = elements[1].getMethodName();
+        String pre = "类:" + className + "-方法:" + methodName + ": ";
+        Log.d(tag, pre + info);
+    }
+
+}
