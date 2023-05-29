8f1b4356073fa36ef6f0a85c72fbeb473d9e6d3f
1146699396@qq.com <t13641435978>
2023-05-30 00:26:05 +0800

    mvvm

--- app/src/main/java/com/ljwx/basemodule/vm/TestViewModel.kt
+++ app/src/main/java/com/ljwx/basemodule/vm/TestViewModel.kt
@@ -1,5 +1,6 @@
 package com.ljwx.basemodule.vm
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.ljwx.baseapp.vm.BaseViewModel
 
@@ -15,6 +16,21 @@ class TestViewModel : BaseViewModel() {
 
     fun requestTest() {
         mTestRepository?.requestTest()
+    }
+
+    //https://developer.android.com/topic/libraries/architecture/viewmodel?hl=zh-cn
+    private val users: MutableLiveData<List<TestData>> by lazy {
+        MutableLiveData<List<TestData>>().also {
+            loadUsers()
+        }
+    }
+
+    fun getUsers(): LiveData<List<TestData>> {
+        return users
+    }
+
+    private fun loadUsers() {
+        // Do an asynchronous operation to fetch users.
     }
 
 }
