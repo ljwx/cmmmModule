70222953bf94ad2482adba084924d902553c3b8b
1146699396@qq.com <t13641435978>
2023-06-25 00:20:39 +0800

    [opt] 添加屏幕方向设置

--- app/src/main/java/com/ljwx/basemodule/MainActivity.kt
+++ app/src/main/java/com/ljwx/basemodule/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ljwx.basemodule
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import com.ljwx.baseactivity.fast.FastMainActivity
 import com.ljwx.baseapp.vm.BaseViewModel
@@ -15,7 +16,6 @@ class MainActivity : FastMainActivity<ActivityMainBinding, EmptyViewModel>(R.lay
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         addTabFragment("vmFragment", ViewModelFragment())
         addTabFragment("toolbar", BaseToolbarFragment())
         addTabFragment("basefragment", BaseFragmentTest())
@@ -23,4 +23,7 @@ class MainActivity : FastMainActivity<ActivityMainBinding, EmptyViewModel>(R.lay
         addTabFragment("javaTest", TestJavaFragment(0))
 
     }
+
+    override fun getScreenOrientation() = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+
 }
