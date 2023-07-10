86dd8911c1321d282719f4e4ce7a6a5ff3187a87
ljwx <1146699396@qq.com>
2023-07-10 06:35:24 +0000

    调整优化

--- baseapp/src/main/java/com/ljwx/baseapp/response/BaseResponse.kt
+++ baseapp/src/main/java/com/ljwx/baseapp/response/BaseResponse.kt
@@ -2,6 +2,13 @@ package com.ljwx.baseapp.response
 
 sealed class BaseResponse<out T : Any> {
 
+    var code: Int? = 0
+
+    @Deprecated(message="useless", replaceWith = ReplaceWith(expression = "msg"))
+    var message: String? = ""
+
+    var msg: String? = ""
+
     data class Success<out T : Any>(val data: T) : BaseResponse<T>()
     data class Error(val exception: ResponseException) : BaseResponse<Nothing>()
 
