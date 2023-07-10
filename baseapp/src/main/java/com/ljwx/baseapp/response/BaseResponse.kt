3f83fb4fae29decee8b4894ad934d157d51ee11f
ljwx <1146699396@qq.com>
2023-07-10 06:52:19 +0000

    调整优化

--- baseapp/src/main/java/com/ljwx/baseapp/response/BaseResponse.kt
+++ baseapp/src/main/java/com/ljwx/baseapp/response/BaseResponse.kt
@@ -2,15 +2,19 @@ package com.ljwx.baseapp.response
 
 sealed class BaseResponse<out T : Any> {
 
-    var code: Int? = 0
+    var code: Int? = null
 
     @Deprecated(message="useless", replaceWith = ReplaceWith(expression = "msg"))
-    var message: String? = ""
+    var message: String? = null
 
-    var msg: String? = ""
+    var msg: String? = null
 
     data class Success<out T : Any>(val data: T) : BaseResponse<T>()
     data class Error(val exception: ResponseException) : BaseResponse<Nothing>()
+
+    open fun isSuccess(): Boolean {
+        return code == 200
+    }
 
     override fun toString(): String {
         return when (this) {
