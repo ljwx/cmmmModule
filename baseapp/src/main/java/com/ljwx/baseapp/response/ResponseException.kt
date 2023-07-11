00c8ad6a2420ae2c014c734781c029695754f3ca
ljwx <1146699396@qq.com>
2023-07-11 02:11:47 +0000

    调整优化

--- baseapp/src/main/java/com/ljwx/baseapp/response/ResponseException.kt
+++ baseapp/src/main/java/com/ljwx/baseapp/response/ResponseException.kt
@@ -1,7 +1,7 @@
 package com.ljwx.baseapp.response
 
-open class ResponseException : Exception() {
-
-    var exceptionCode: Int = 0
-
-}
+open class ResponseException @JvmOverloads constructor(
+    private val throwable: Throwable,
+    val customCode: Int? = null,
+    val customMessage: String? = null,
+) : Exception(throwable)
--- baseapp/src/main/java/com/ljwx/baseapp/vm/BaseDataRepository.kt
+++ baseapp/src/main/java/com/ljwx/baseapp/vm/BaseDataRepository.kt
@@ -1,8 +1,21 @@
 package com.ljwx.baseapp.vm
 
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
 
 open class BaseDataRepository {
 
+    private var mCompositeDisposable: CompositeDisposable? = null
 
+    open fun autoClear(disposable: Disposable) {
+        if (mCompositeDisposable == null) {
+            mCompositeDisposable = CompositeDisposable()
+        }
+        mCompositeDisposable?.add(disposable)
+    }
+
+    open fun onCleared() {
+        mCompositeDisposable?.clear()
+    }
 
 }
