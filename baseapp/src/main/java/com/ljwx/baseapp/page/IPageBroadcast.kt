91062872899291a23b131d4837966b4130fb14e7
1146699396@qq.com <t13641435978>
2023-07-15 00:33:29 +0800

    调整优化

--- baseapp/src/main/java/com/ljwx/baseapp/page/IPageBroadcast.kt
+++ baseapp/src/main/java/com/ljwx/baseapp/page/IPageBroadcast.kt
@@ -6,13 +6,19 @@ interface IPageBroadcast {
 
     fun registerRefreshBroadcast(vararg actions: String?)
 
+    fun registerOtherBroadcast(action: String)
+
     fun unregisterBroadcast(action: String?)
 
     fun sendFinishBroadcast(action: String?)
 
     fun sendRefreshBroadcast(action: String?, params: String? = null)
 
+    fun sendOtherBroadcast(action: String)
+
     fun onBroadcastPageFinish()
 
     fun onBroadcastPageRefresh(type: String?)
+
+    fun onBroadcastOther()
 }
