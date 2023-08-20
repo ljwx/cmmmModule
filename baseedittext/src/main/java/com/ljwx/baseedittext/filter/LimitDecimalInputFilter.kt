f9525e98ede8aa5421ac9d371f7fa3892003e42b
ljwx <1146699396@qq.com>
2023-08-21 00:45:46 +0800

    调整优化

---
+++ baseedittext/src/main/java/com/ljwx/baseedittext/filter/LimitDecimalInputFilter.kt
@@ -0,0 +1,42 @@
+package com.ljwx.baseedittext.filter
+
+import android.text.InputFilter
+import android.text.Spanned
+
+class LimitDecimalInputFilter(private val digits: Int = 2) : InputFilter {
+
+    override fun filter(
+        source: CharSequence,
+        start: Int,
+        end: Int,
+        dest: Spanned,
+        dstart: Int,
+        dend: Int,
+    ): CharSequence? {
+        if (end != 0) {
+            //直接输入.
+            if (dest.isEmpty() && source == ".") {
+                return "0."
+            }
+            if (dest.contains(".")) {
+                val pointIndex = dest.indexOf(".")
+                //在小数点之前输入数字
+                if (dstart <= pointIndex && dend <= pointIndex) {
+                    return null
+                }
+                //已经有两位小数
+                if (dest.substring(pointIndex + 1).length >= digits) {
+                    return ""
+                }
+            } else {
+                //在前面输入. 暂时不让输
+                if (source.contains(".")) {
+                    if (dest.length - digits > dend) {
+                        return ""
+                    }
+                }
+            }
+        }
+        return null
+    }
+}
