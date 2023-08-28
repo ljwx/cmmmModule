6b1a9d4a4d58f88ad93e67823ef6d8b6c1451607
ljwx <1146699396@qq.com>
2023-08-28 19:47:59 +0800

    优化调整

---
+++ basenetwork/src/main/java/com/ljwx/basenetwork/rxjava/ResponseThrowable.java
@@ -0,0 +1,13 @@
+package com.sisensing.http;
+
+
+
+public class ResponseThrowable extends Exception {
+    public int code;
+    public String message;
+
+    public ResponseThrowable(Throwable throwable, int code) {
+        super(throwable);
+        this.code = code;
+    }
+}
---
+++ basenetwork/src/main/java/com/ljwx/basenetwork/rxjava/RxUtils.java
@@ -0,0 +1,34 @@
+public class RxUtils {
+    /**
+     * 线程调度器
+     */
+    public static ObservableTransformer schedulersTransformer() {
+        return new ObservableTransformer() {
+            @Override
+            public ObservableSource apply(Observable upstream) {
+                return upstream.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+    public static ObservableTransformer exceptionTransformer() {
+
+        return new ObservableTransformer() {
+            @Override
+            public ObservableSource apply(Observable observable) {
+                return observable
+//                        .map(new HandleFuc<T>())  //这里可以取出BaseResponse中的Result
+                        .onErrorResumeNext(new HttpResponseFunc());
+            }
+        };
+    }
+
+    private static class HttpResponseFunc<T> implements Function<Throwable, Observable<T>> {
+        @Override
+        public Observable<T> apply(Throwable t) {
+            return Observable.error(ExceptionHandle.handleException(t));
+        }
+    }
+
+}
