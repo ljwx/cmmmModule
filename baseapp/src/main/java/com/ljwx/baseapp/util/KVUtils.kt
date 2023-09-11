f535b51468a41cf1f69c6136d4c5131fcb2082fe
zhm.tian <zhm.tian@sibionics.com>
2023-09-11 13:43:40 +0800

    调整优化

--- baseapp/src/main/java/com/ljwx/baseapp/util/KVUtils.kt
+++ baseapp/src/main/java/com/ljwx/baseapp/util/KVUtils.kt
@@ -4,26 +4,26 @@ import com.blankj.utilcode.util.SPUtils
 
 class KVUtils {
 
-    companion object {
-
-        private val instance by lazy {
-            KVUtils()
-        }
-
-        @JvmStatic
-        @JvmOverloads
-        fun getInstance(): KVUtils {
-            return instance
-        }
-    }
-
-    private val spUtils by lazy {
-        SPUtils.getInstance()
-    }
-
-    fun put() {
-
-    }
+//    companion object {
+//
+//        private val instance by lazy {
+//            KVUtils()
+//        }
+//
+//        @JvmStatic
+//        @JvmOverloads
+//        fun getInstance(): KVUtils {
+//            return instance
+//        }
+//    }
+//
+//    private val spUtils by lazy {
+//        SPUtils.getInstance()
+//    }
+//
+//    fun put() {
+//
+//    }
 
 
 }
