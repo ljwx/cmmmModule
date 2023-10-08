0e0983539411c02475b0b1d4b29608d8522f1687
zhm.tian <zhm.tian@sibionics.com>
2023-10-08 17:45:14 +0800

    调整优化

--- app/src/main/java/com/ljwx/basemodule/fragments/TestDialog.kt
+++ app/src/main/java/com/ljwx/basemodule/fragments/TestDialog.kt
@@ -1,9 +1,8 @@
 package com.ljwx.basemodule.fragments
 
-import com.ljwx.basedialog.BaseBindingDialogFragment
+import com.ljwx.basedialog.dialogfragment.BaseBindingDialogFragment
 import com.ljwx.basemodule.R
 import com.ljwx.basemodule.databinding.DialogTestBinding
-import com.ljwx.basemodule.databinding.FragmentBaseFragmentBinding
 
 class TestDialog :
     BaseBindingDialogFragment<DialogTestBinding>() {
