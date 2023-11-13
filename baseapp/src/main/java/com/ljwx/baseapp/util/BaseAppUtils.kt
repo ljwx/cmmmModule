5ebf27ef8aa0a53ea2f2e6b812bd100711740781
zhm.tian <zhm.tian@sibionics.com>
2023-11-13 10:14:26 +0800

    调整优化

--- baseapp/src/main/java/com/ljwx/baseapp/util/BaseAppUtils.kt
+++ baseapp/src/main/java/com/ljwx/baseapp/util/BaseAppUtils.kt
@@ -1,6 +1,7 @@
 package com.ljwx.baseapp.util
 
 import android.app.Application
+import androidx.lifecycle.ViewModelStoreOwner
 
 object BaseAppUtils {
 
@@ -12,6 +13,18 @@ object BaseAppUtils {
 
     fun getApplication(): Application {
         return application
+    }
+
+    fun getApplicationViewModelStore(): ViewModelStoreOwner {
+        if (application is ViewModelStoreOwner) {
+            return application as ViewModelStoreOwner
+        }
+        //1.application implements ViewModelStoreOwner
+        //2.onCreate{ mAppViewModelStore = new ViewModelStore() }
+        //3.public ViewModelStore getViewModelStore() {
+        //    return mAppViewModelStore;
+        //}
+        throw IllegalAccessException("Application need implement ViewModelStoreOwner")
     }
 
 }
--- baseapp/src/main/java/com/ljwx/baseapp/vm/ViewModelScope.kt
+++ baseapp/src/main/java/com/ljwx/baseapp/vm/ViewModelScope.kt
@@ -4,11 +4,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.ljwx.baseapp.util.BaseAppUtils
 
 class ViewModelScope {
 
     private var mFragmentProvider: ViewModelProvider? = null
     private var mActivityProvider: ViewModelProvider? = null
+    private var mApplicationProvider: ViewModelProvider? = null
 
     fun <T : ViewModel> getFragmentScopeViewModel(fragment: Fragment, modelClass: Class<T>): T {
         if (mFragmentProvider == null) mFragmentProvider = ViewModelProvider(fragment)
@@ -21,6 +23,11 @@ class ViewModelScope {
     ): T {
         if (mActivityProvider == null) mActivityProvider = ViewModelProvider(activity)
         return mActivityProvider!!.get<T>(modelClass)
+    }
+
+    fun <T : ViewModel> getApplicationScopeViewModel(modelClass: Class<T>): T {
+        if (mActivityProvider == null) mActivityProvider = ViewModelProvider(BaseAppUtils.getApplicationViewModelStore())
+        return mApplicationProvider!!.get<T>(modelClass)
     }
 
 }
