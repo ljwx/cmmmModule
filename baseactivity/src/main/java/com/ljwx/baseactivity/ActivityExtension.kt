46c4f5b73f0d9b9b745c8e4c08d5b39d3b0c7b1a
zhm.tian <zhm.tian@sibionics.com>
2023-11-22 20:16:57 +0800

    调整优化

--- baseactivity/src/main/java/com/ljwx/baseactivity/ActivityExtension.kt
+++ baseactivity/src/main/java/com/ljwx/baseactivity/ActivityExtension.kt
@@ -10,7 +10,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.lifecycleScope
 import com.ljwx.baseapp.constant.BaseConstBundleKey
-import com.ljwx.baseapp.debug.DebugUtils
 import com.ljwx.baseapp.debug.ILogCheckRecyclerView
 import com.ljwx.baseapp.extensions.visibleGone
 import com.ljwx.baseapp.shake.registerShake
