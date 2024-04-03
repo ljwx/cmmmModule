483c298eeb7832c551f37770d8675a6c96311a49
tianchaoming <tianchaoming@codemao.cn>
2024-04-03 10:29:16 +0800

    调整优化

--- baseapp/src/main/java/com/ljwx/baseapp/extensions/String.kt
+++ baseapp/src/main/java/com/ljwx/baseapp/extensions/String.kt
@@ -3,7 +3,7 @@ package com.ljwx.baseapp.extensions
 import com.ljwx.baseapp.regex.CommonRegex
 import java.util.regex.Pattern
 
-fun CharSequence?.isMatch(regex: String):Boolean {
+fun CharSequence?.isMatch(regex: String): Boolean {
     return !this.isNullOrEmpty() && Pattern.matches(regex, this)
 }
 
@@ -11,14 +11,22 @@ inline fun CharSequence?.notNullOrBlank(): Boolean {
     return !isNullOrBlank()
 }
 
-fun CharSequence?.isEmail() :Boolean{
+fun CharSequence?.isEmail(): Boolean {
     return this.isMatch(CommonRegex.email)
 }
 
-fun CharSequence?.isInt() :Boolean{
+fun CharSequence?.isInt(): Boolean {
     return this.isMatch(CommonRegex.integer)
 }
 
-fun CharSequence?.isFloat() :Boolean{
+fun CharSequence?.isFloat(): Boolean {
     return this.isMatch(CommonRegex.decimal)
+}
+
+fun String?.safeToInt(default: Int): Int {
+    return if (isInt()) {
+        this!!.toInt()
+    } else {
+        default
+    }
 }
